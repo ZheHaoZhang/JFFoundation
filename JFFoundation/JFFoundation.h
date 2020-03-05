6591b5a8458554c04797a097f2d66a59abbef6d1
張哲豪 <ds73100@gmail.com>
2020-03-05 15:14:52 +0800

    git init

---
+++ JFFoundation/JFFoundation.h
@@ -0,0 +1,19 @@
+//
+//  JFFoundation.h
+//  JFFoundation
+//
+//  Created by 張哲豪 on 2020/3/5.
+//  Copyright © 2020 張哲豪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JFFoundation.
+FOUNDATION_EXPORT double JFFoundationVersionNumber;
+
+//! Project version string for JFFoundation.
+FOUNDATION_EXPORT const unsigned char JFFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFFoundation/PublicHeader.h>
+
+
